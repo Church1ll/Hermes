715dc6f3ce556b549fa66a7393a19218ad6f6df9
Churchill <cristian.musteata.e@gmail.com>
2026-05-18 21:30:18 +0300

    update demo page

--- projects/demo/src/app/app.routes.ts
+++ projects/demo/src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { Routes } from '@angular/router';
 import { CrossTabPage } from './pages/cross-tab-page/cross-tab-page';
 import { HomePage } from './pages/home-page/home-page';
 import { LifecyclePage } from './pages/lifecycle-page/lifecycle-page';
-import { MetricsPage } from './pages/metrics-page/metrics-page';
 import { ReplayPage } from './pages/replay-page/replay-page';
 import { TopicBusPage } from './pages/topic-bus-page/topic-bus-page';
 import { WildcardsPage } from './pages/wildcards-page/wildcards-page';
@@ -14,6 +13,5 @@ export const routes: Routes = [
   { path: 'replay', component: ReplayPage },
   { path: 'lifecycle', component: LifecyclePage },
   { path: 'cross-tab', component: CrossTabPage },
-  { path: 'metrics', component: MetricsPage },
   { path: '**', redirectTo: '' },
 ];
